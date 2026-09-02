5d317545ffb586f7da4c88e74911847f8b841feb
Gaurav Kumar <workwithgaurav000>
2026-09-02 18:45:01 +0530

    Configure Render port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // ==========================================
 // FILE PATHS
